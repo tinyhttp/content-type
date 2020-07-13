b0b8a5b77102e72dfb2f179ec5deb26ca2256715
v1rtl <pilll.PL22@gmail.com>
2020-07-13 03:24:20 +0300

    write the module

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # es-content-type
-content-type rewrite in TypeScript with ESM and CommonJS targets
+
+[![Twitter](https://img.shields.io/twitter/follow/v1rtl.svg?label=twitter&style=flat-square)](https://twitter.com/v1rtl)
+![Top lang](https://img.shields.io/github/languages/top/talentlessguy/es-content-type.svg?style=flat-square)
+![Vulnerabilities](https://img.shields.io/snyk/vulnerabilities/npm/es-content-type.svg?style=flat-square)
+[![Version](https://img.shields.io/npm/v/es-content-type.svg?style=flat-square)](https://npm.im/es-content-type)
+![Last commit](https://img.shields.io/github/last-commit/talentlessguy/es-content-type.svg?style=flat-square)
+
+> [`content-type`](https://github.com/jshttp/content-type) rewrite in TypeScript with ESM and CommonJS targets.
+
+Create and parse HTTP Content-Type header according to RFC 7231
+
+## Install
+
+```sh
+pnpm i es-content-type
+```
+
+## API
+
+```ts
+import { parse, format } from 'es-content-type'
+```
+
+### `parse(string: string | Request | Response)`
+
+```ts
+const obj = parse('image/svg+xml; charset=utf-8')
+```
+
+Parse a `Content-Type` header. This will return an object with the following
+properties (examples are shown for the string `'image/svg+xml; charset=utf-8'`):
+
+- `type`: The media type (the type and subtype, always lower case).
+  Example: `'image/svg+xml'`
+
+- `parameters`: An object of the parameters in the media type (name of parameter
+  always lower case). Example: `{charset: 'utf-8'}`
+
+Throws a `TypeError` if the string is missing or invalid.
+
+```ts
+const obj = contentType.parse(req)
+```
+
+Parse the `Content-Type` header from the given `req`. Short-cut for
+`contentType.parse(req.headers['content-type'])`.
+
+Throws a `TypeError` if the `Content-Type` header is missing or invalid.
+
+```js
+const obj = contentType.parse(res)
+```
+
+Parse the `Content-Type` header set on the given `res`. Short-cut for
+`contentType.parse(res.getHeader('content-type'))`.
+
+Throws a `TypeError` if the `Content-Type` header is missing or invalid.
+
+### `format(obj)`
+
+```ts
+const str = contentType.format({
+  type: 'image/svg+xml',
+  parameters: { charset: 'utf-8' },
+})
+```
+
+Format an object into a `Content-Type` header. This will return a string of the
+content type for the given object with the following properties (examples are
+shown that produce the string `'image/svg+xml; charset=utf-8'`):
+
+- `type`: The media type (will be lower-cased). Example: `'image/svg+xml'`
+
+- `parameters`: An object of the parameters in the media type (name of the
+  parameter will be lower-cased). Example: `{charset: 'utf-8'}`
+
+Throws a `TypeError` if the object contains an invalid type or parameter names.
+
+## License
+
+MIT © [v1rtl](https://v1rtl.site)
---
+++ src/index.ts
@@ -0,0 +1,176 @@
+import { IncomingMessage as Request, ServerResponse as Response } from 'http'
+
+/**
+ * RegExp to match *( ";" parameter ) in RFC 7231 sec 3.1.1.1
+ *
+ * parameter     = token "=" ( token / quoted-string )
+ * token         = 1*tchar
+ * tchar         = "!" / "#" / "$" / "%" / "&" / "'" / "*"
+ *               / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
+ *               / DIGIT / ALPHA
+ *               ; any VCHAR, except delimiters
+ * quoted-string = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+ * qdtext        = HTAB / SP / %x21 / %x23-5B / %x5D-7E / obs-text
+ * obs-text      = %x80-FF
+ * quoted-pair   = "\" ( HTAB / SP / VCHAR / obs-text )
+ */
+const PARAM_REGEXP = /; *([!#$%&'*+.^_`|~0-9A-Za-z-]+) *= *("(?:[\u000b\u0020\u0021\u0023-\u005b\u005d-\u007e\u0080-\u00ff]|\\[\u000b\u0020-\u00ff])*"|[!#$%&'*+.^_`|~0-9A-Za-z-]+) */g
+const TEXT_REGEXP = /^[\u000b\u0020-\u007e\u0080-\u00ff]+$/
+const TOKEN_REGEXP = /^[!#$%&'*+.^_`|~0-9A-Za-z-]+$/
+
+/**
+ * RegExp to match quoted-pair in RFC 7230 sec 3.2.6
+ *
+ * quoted-pair = "\" ( HTAB / SP / VCHAR / obs-text )
+ * obs-text    = %x80-FF
+ */
+const QESC_REGEXP = /\\([\u000b\u0020-\u00ff])/g
+
+/**
+ * RegExp to match chars that must be quoted-pair in RFC 7230 sec 3.2.6
+ */
+const QUOTE_REGEXP = /([\\"])/g
+
+/**
+ * RegExp to match type in RFC 7231 sec 3.1.1.1
+ *
+ * media-type = type "/" subtype
+ * type       = token
+ * subtype    = token
+ */
+const TYPE_REGEXP = /^[!#$%&'*+.^_`|~0-9A-Za-z-]+\/[!#$%&'*+.^_`|~0-9A-Za-z-]+$/
+
+function qstring(val: any) {
+  const str = String(val)
+
+  // no need to quote tokens
+  if (TOKEN_REGEXP.test(str)) {
+    return str
+  }
+
+  if (str.length > 0 && !TEXT_REGEXP.test(str)) {
+    throw new TypeError('invalid parameter value')
+  }
+
+  return '"' + str.replace(QUOTE_REGEXP, '\\$1') + '"'
+}
+
+function getcontenttype(obj: Request | Response) {
+  let header: string
+
+  if (obj instanceof Response && typeof obj.getHeader === 'function') {
+    // res-like
+    header = obj.getHeader('content-type') as string
+  } else if (obj instanceof Request && typeof obj.headers === 'object') {
+    // req-like
+    const h = obj.headers
+    header = h && h['content-type']
+  }
+
+  if (typeof header !== 'string') {
+    throw new TypeError('content-type header is missing from object')
+  }
+
+  return header
+}
+
+/**
+ * Class to represent a content type.
+ */
+class ContentType {
+  parameters: any
+  type: string
+  constructor(type: string) {
+    this.parameters = {}
+    this.type = type
+  }
+}
+
+/**
+ * Format object to media type.
+ */
+export function format(obj: any) {
+  if (!obj || typeof obj !== 'object') {
+    throw new TypeError('argument obj is required')
+  }
+
+  const { parameters, type } = obj
+
+  if (!type || !TYPE_REGEXP.test(type)) {
+    throw new TypeError('invalid type')
+  }
+
+  let string = type
+
+  // append parameters
+  if (parameters && typeof parameters == 'object') {
+    const params = Object.keys(parameters).sort()
+
+    for (const param of params) {
+      if (!TOKEN_REGEXP.test(param)) {
+        throw new TypeError('invalid parameter name')
+      }
+
+      string += '; ' + param + '=' + qstring(parameters[param])
+    }
+  }
+
+  return string
+}
+
+/**
+ * Parse media type to object.
+ */
+export function parse(string: string | Request | Response) {
+  if (!string) {
+    throw new TypeError('argument string is required')
+  }
+
+  // support req/res-like objects as argument
+  const header = typeof string == 'object' ? getcontenttype(string) : string
+
+  if (typeof header !== 'string') {
+    throw new TypeError('argument string is required to be a string')
+  }
+
+  let index = header.indexOf(';')
+  const type = index != -1 ? header.substr(0, index).trim() : header.trim()
+
+  if (!TYPE_REGEXP.test(type)) {
+    throw new TypeError('invalid media type')
+  }
+
+  const obj = new ContentType(type.toLowerCase())
+
+  // parse parameters
+  if (index != -1) {
+    let key: string
+    let match: RegExpExecArray
+    let value: string
+
+    PARAM_REGEXP.lastIndex = index
+
+    while ((match = PARAM_REGEXP.exec(header))) {
+      if (match.index !== index) {
+        throw new TypeError('invalid parameter format')
+      }
+
+      index += match[0].length
+      key = match[1].toLowerCase()
+      value = match[2]
+
+      if (value[0] == '"') {
+        // remove quotes and escapes
+        value = value.substr(1, value.length - 2).replace(QESC_REGEXP, '$1')
+      }
+
+      obj.parameters[key] = value
+    }
+
+    if (index != header.length) {
+      throw new TypeError('invalid parameter format')
+    }
+  }
+
+  return obj
+}
