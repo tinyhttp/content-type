4ffb85551c3d3984ea303cf6421269ec6852156e
v1rtl <pilll.PL22@gmail.com>
2020-07-13 04:20:48 +0300

    yeet bili and use rollup instead

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+const ts = require('rollup-plugin-typescript2')
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+    },
+  ],
+  plugins: [ts()],
+  external: ['http'],
+}
